0818bcb438f7011ab1517a585f394252327c0fa4
Shakyansh Maurya <126961167+Uzumakinaruto6384@users.noreply.github.com>
2026-02-19 19:06:16 +0530

    Day5_Count_Even_Odd.c

---
+++ Day5_Count_Even_Odd.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, even = 0, odd = 0;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even count = %d\n", even);
+    printf("Odd count = %d", odd);
+
+    return 0;
+}
