a278deb8738802cd2170e6e664d5caa10944ff4b
Shakyansh Maurya <126961167+Uzumakinaruto6384@users.noreply.github.com>
2026-03-01 20:13:02 +0530

    Fibonacci_Loop.c

---
+++ Day11_Fibonacci_Loop.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int first = 0, second = 1, next;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series:\n");
+
+    for(i = 0; i < n; i++) {
+        if(i == 0)
+            printf("%d ", first);
+        else if(i == 1)
+            printf("%d ", second);
+        else {
+            next = first + second;
+            printf("%d ", next);
+            first = second;
+            second = next;
+        }
+    }
+
+    return 0;
+}
