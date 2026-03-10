cbfc8a21dae08871ef0d43da0f000dd9146ecb9c
Shakyansh Maurya <126961167+Uzumakinaruto6384@users.noreply.github.com>
2026-03-10 19:30:22 +0530

    Reverse_Array.c

---
+++ Day21_Reverse_Array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[5] = {10, 20, 30, 40, 50};
+    int i;
+
+    printf("Reversed Array:\n");
+
+    for(i = 4; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
