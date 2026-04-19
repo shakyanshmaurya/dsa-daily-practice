1dceff92bdc5f5b93b4ec85b00b848c424637d08
Shakyansh Maurya <126961167+Uzumakinaruto6384@users.noreply.github.com>
2026-04-19 13:34:59 +0530

    Day59_Move_Zeros.c

---
+++ Day59_Move_Zeros.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[] = {0, 1, 0, 3, 12};
+    int n = 5;
+
+    int j = 0;
+
+    for(int i = 0; i < n; i++) {
+
+        if(arr[i] != 0) {
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            j++;
+        }
+    }
+
+    printf("Array after moving zeros:\n");
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
