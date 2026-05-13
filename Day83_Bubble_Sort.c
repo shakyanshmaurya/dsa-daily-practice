50245c8eaa064e91e3972a3ecdd770576a8260b4
Shakyansh Maurya <126961167+Uzumakinaruto6384@users.noreply.github.com>
2026-05-13 22:56:17 +0530

    Day83_Bubble_Sort.c

---
+++ Day83_Bubble_Sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[] = {5, 1, 4, 2, 8};
+    int n = 5;
+
+    for(int i = 0; i < n - 1; i++) {
+
+        for(int j = 0; j < n - i - 1; j++) {
+
+            if(arr[j] > arr[j + 1]) {
+
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted Array:\n");
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
