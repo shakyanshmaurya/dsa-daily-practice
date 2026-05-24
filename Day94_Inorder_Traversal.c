ac041239906018c8fd22e41dc9414d812f7c5c1a
Shakyansh Maurya <126961167+shakyanshmaurya@users.noreply.github.com>
2026-05-24 19:04:05 +0530

    Day94_Inorder_Traversal.c

---
+++ Day94_Inorder_Traversal.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// node structure
+struct Node {
+
+    int data;
+
+    struct Node* left;
+    struct Node* right;
+};
+
+// create node
+struct Node* createNode(int value) {
+
+    struct Node* newNode =
+        (struct Node*) malloc(sizeof(struct Node));
+
+    newNode->data = value;
+
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+// inorder traversal
+void inorder(struct Node* root) {
+
+    if(root == NULL) {
+        return;
+    }
+
+    inorder(root->left);
+
+    printf("%d ", root->data);
+
+    inorder(root->right);
+}
+
+int main() {
+
+    struct Node* root = createNode(10);
+
+    root->left = createNode(20);
+    root->right = createNode(30);
+
+    root->left->left = createNode(40);
+    root->left->right = createNode(50);
+
+    printf("Inorder Traversal:\n");
+
+    inorder(root);
+
+    return 0;
+}
