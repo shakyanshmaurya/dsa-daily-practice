24c0313baf935beabe29ee229576cccdbcddaa3e
Shakyansh Maurya <126961167+shakyanshmaurya@users.noreply.github.com>
2026-06-02 20:23:39 +0530

    Day101_Find_Minimum_BST.c

---
+++ Day101_Find_Minimum_BST.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int value) {
+
+    struct Node* newNode =
+        (struct Node*)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int value) {
+
+    if(root == NULL)
+        return createNode(value);
+
+    if(value < root->data)
+        root->left = insert(root->left, value);
+
+    else if(value > root->data)
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+int findMinimum(struct Node* root) {
+
+    if(root == NULL) {
+        return -1;
+    }
+
+    while(root->left != NULL) {
+        root = root->left;
+    }
+
+    return root->data;
+}
+
+int main() {
+
+    struct Node* root = NULL;
+
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 70);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 10);
+
+    printf("Minimum Element = %d",
+            findMinimum(root));
+
+    return 0;
+}
