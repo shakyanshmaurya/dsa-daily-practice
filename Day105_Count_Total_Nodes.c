fe166e07b14b628ddd6a05de54efb405dcb535a2
Shakyansh Maurya <126961167+shakyanshmaurya@users.noreply.github.com>
2026-06-07 20:21:55 +0530

    Day105_Count_Total_Nodes.c

---
+++ Day105_Count_Total_Nodes.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int value) {
+
+    struct Node* newNode =
+        (struct Node*)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+// Count Nodes Function
+int countNodes(struct Node* root) {
+
+    if(root == NULL)
+        return 0;
+
+    return 1 +
+           countNodes(root->left) +
+           countNodes(root->right);
+}
+
+int main() {
+
+    struct Node* root = createNode(1);
+
+    root->left = createNode(2);
+    root->right = createNode(3);
+
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    printf("Total Nodes = %d",
+           countNodes(root));
+
+    return 0;
+}
