b0cbfcd3fa9855550f2593b8fd113d01c84013bf
Shakyansh Maurya <126961167+shakyanshmaurya@users.noreply.github.com>
2026-06-08 19:56:16 +0530

    Day106_Count_Leaf_Nodes.c

---
+++ Day106_Count_Leaf_Nodes.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int value) {
+
+    struct Node* newNode =
+        (struct Node*)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+int countLeafNodes(struct Node* root) {
+
+    if(root == NULL)
+        return 0;
+
+    if(root->left == NULL &&
+       root->right == NULL)
+        return 1;
+
+    return countLeafNodes(root->left)
+         + countLeafNodes(root->right);
+}
+
+int main() {
+
+    struct Node* root = createNode(1);
+
+    root->left = createNode(2);
+    root->right = createNode(3);
+
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    printf("Leaf Nodes = %d",
+           countLeafNodes(root));
+
+    return 0;
+}
