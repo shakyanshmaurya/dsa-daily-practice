f7f1f3ad30fb64eac10c73dda02b8703421bf654
Shakyansh Maurya <126961167+shakyanshmaurya@users.noreply.github.com>
2026-08-19 19:41:56 +0530

    Day163_Count_Complete_Binary_Tree_Nodes.c

---
+++ Day163_Count_Complete_Binary_Tree_Nodes.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node* createNode(int value) {
+
+    struct Node *newNode =
+        (struct Node*)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+int countNodes(struct Node *root) {
+
+    if(root == NULL)
+        return 0;
+
+    return 1 +
+           countNodes(root->left) +
+           countNodes(root->right);
+}
+
+int main() {
+
+    struct Node *root = createNode(1);
+
+    root->left = createNode(2);
+    root->right = createNode(3);
+
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    root->right->left = createNode(6);
+
+    int total = countNodes(root);
+
+    printf("Total Nodes = %d", total);
+
+    return 0;
+}
