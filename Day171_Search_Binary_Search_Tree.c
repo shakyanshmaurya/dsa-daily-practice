57c7466fa8eeddc17efc33434d51a2c9cdadb5bd
Shakyansh Maurya <126961167+shakyanshmaurya@users.noreply.github.com>
2026-08-31 21:27:05 +0530

    Day171_Search_Binary_Search_Tree.c

---
+++ Day171_Search_Binary_Search_Tree.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node* createNode(int value) {
+
+    struct Node *newNode =
+        (struct Node*)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+struct Node* insert(struct Node *root, int value) {
+
+    if(root == NULL)
+        return createNode(value);
+
+    if(value < root->data)
+        root->left = insert(root->left, value);
+
+    else if(value > root->data)
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+/* Search in BST */
+
+struct Node* search(struct Node *root, int key) {
+
+    if(root == NULL)
+        return NULL;
+
+    if(root->data == key)
+        return root;
+
+    if(key < root->data)
+        return search(root->left, key);
+
+    return search(root->right, key);
+}
+
+int main() {
+
+    struct Node *root = NULL;
+
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 70);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 60);
+    root = insert(root, 80);
+
+    int key = 60;
+
+    struct Node *result = search(root, key);
+
+    if(result != NULL)
+        printf("%d found in BST", key);
+    else
+        printf("%d not found in BST", key);
+
+    return 0;
+}
